d94f514ec2573c3a9acf45e38358adc9c2c0a8cf
OmarAreef <omar.mousaaref@gmail.com>
2022-11-20 01:51:49 +0100

    Added state and genGrid fuction

--- src/CoastGuard.java
+++ src/CoastGuard.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CoastGuard extends searchProblem {
 
@@ -21,7 +22,65 @@ public class CoastGuard extends searchProblem {
     }
 
     public static String genGrid() {
+        int M = (int) (Math.random() * 5 + 1);
+        int N = (int) (Math.random() * 15 + 1);
+
+        int C = (int) (Math.random() * 70 + 30);
+
+        int cgX = (int) (Math.random() * M);
+        int cgY = (int) (Math.random() * N);
+
+        ArrayList<String> stationLocations = generateLocations(M, N, cgX, cgY, new ArrayList<>());
+        ArrayList<String> shipLocations = generateLocations(M, N, cgX, cgY, stationLocations);
+
+        String shipLocationsAndPassengerNumbers = "";
+        for (String shipString : shipLocations) {
+            int numberOfPassengerNumbers = (int) (Math.random() * 100);
+            shipLocationsAndPassengerNumbers = shipLocationsAndPassengerNumbers + shipString + ","
+                    + numberOfPassengerNumbers + ",";
+        }
+        shipLocationsAndPassengerNumbers = shipLocationsAndPassengerNumbers.substring(0,
+                shipLocationsAndPassengerNumbers.length() - 1) + ";";
+        String stationStringFinal = "";
+        for (String stationString : stationLocations) {
+            stationStringFinal += stationString + ",";
+        }
+        stationStringFinal = stationStringFinal.substring(0,
+                stationStringFinal.length() - 1);
+        return ("" + M + "," + N + ";" + C + ";" + cgX + "," + cgY + ";" + stationStringFinal + ";"
+                + shipLocationsAndPassengerNumbers);
+    }
+
+    private static ArrayList<String> generateLocations(int M, int N, int cgX, int cgY,
+            ArrayList<String> occupiedLocations) {
+        int numberOfShips = (int) ((Math.random() * (M - 1) * N) + 1);
+        while (numberOfShips > (M * N)) {
+            numberOfShips = (int) ((Math.random() * (M - 1) * N) + 1);
+        }
+        ArrayList<String> stationLocations = new ArrayList<String>();
+        for (int i = 0; i < numberOfShips; i++) {
+            int stationX = (int) (Math.random() * M);
+            int stationY = (int) (Math.random() * N);
+            while ((stationX == cgX && stationY == cgY) || stationLocations.contains(stationX + "," + stationY) ||
+                    occupiedLocations.contains(stationX + "," + stationY)) {
+                stationX = (int) (Math.random() * M);
+                stationY = (int) (Math.random() * N);
+
+            }
+            stationLocations.add(stationX + "," + stationY);
+        }
+        return stationLocations;
+    }
+
+    public static String solve(String grid, String strategy, boolean visualize) {
+
+        
         return "";
+    }
+
+    public static void main(String[] args) {
+        String grid = genGrid();
+        solve(grid, "df", false);
     }
 
 }
--- src/State.java
+++ src/State.java
@@ -1,9 +1,68 @@
-public class State {
-    
-    public int ss;
+import java.util.ArrayList;
 
-    public State(int ss) {
-        this.ss = ss;
+class Ship {
+
+    public int xPos;
+    public int yPos;
+    public int passengers;
+    public int health;
+
+    public Ship(int xPos, int yPos, int passengers, int health) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.passengers = passengers;
+        this.health = health;
+    }
+
+    public void pickUp(int numberOfPassengers) {
+        this.passengers = this.passengers - numberOfPassengers;
+    }
+
+    public void updateShip() {
+        if (this.passengers > 0) {
+            this.passengers = this.passengers - 1;
+            return;
+        }
+        this.health = this.health - 1;
+    }
+}
+
+class Agent {
+
+    public int xPos;
+    public int yPos;
+    public int totalCapacity;
+    public int remainingCapacity;
+
+    public Agent(int xPos, int yPos, int totalCapacity) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.totalCapacity = totalCapacity;
+        this.remainingCapacity = totalCapacity;
+    }
+
+    public void drop() {
+        this.remainingCapacity = 0;
+    }
+
+    public void pickUp(int passengers) {
+        this.remainingCapacity = this.remainingCapacity - passengers;
+    }
+
+}
+
+public class State {
+
+    public ArrayList<Ship> ships;
+    public Agent agent;
+    public int deaths;
+    public int retrievedBlackBoxes;
+
+    public State(ArrayList<Ship> ships, Agent agent, int deaths, int retrieved) {
+        this.ships = ships;
+        this.agent = agent;
+        this.deaths = deaths;
+        this.retrievedBlackBoxes = retrieved;
     }
 
 }
